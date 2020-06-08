9d93f63f40bcac858085f54af181657d2ab20552
Mike Ajala <agmt5989@gmail.com>
2020-06-08 23:27:57 +0100

    starting with user registration route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+// Setup database in knex
+const db = require('knex')({
+	client: 'mysql2',
+	connection: {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_SCHEMA
+	}
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({msg: 'respond with a resource'});
 });
 
 router.post('/add', function(req, res) => {
-	const
+	const { phone, email, fname } = req.body;
+	
+	db('users').where({phone: adminId}).first()
 })
 
 module.exports = router;
