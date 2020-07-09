e2e1b4ff1fde073bb208e3eb4c65960f3ec3c851
Mike Ajala <agmt5989@gmail.com>
2020-07-09 10:21:23 +0100

    allowing cross origin request sharing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
+
+router.use(cors({
+	origin: 'https://powerjamb.ng'
+}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
